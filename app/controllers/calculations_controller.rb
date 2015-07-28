e897459e1259fdcbc3efb5a4e0a2e1c7a2925d59
howabunga <weihowa@gmail.com>
2015-07-27 22:53:42 -0500

    First Draft
    calculations complete, but nothing linked together yet

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,42 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+
+    @number =params["number"]
+    @square = (@number.to_f)**2
+
+    render("square.html.erb")
+  end
+
+  def square_root
+
+    @number2 =params["number2"]
+    @squareroot = Math.sqrt(@number2.to_f)
+
+    render("squareroot.html.erb")
+  end
+
+  def min_max
+
+    @min = params["min"]
+    @max = params["max"]
+
+    @min_max = rand(@min.to_i..@max.to_i)
+
+    render("minmax.html.erb")
+  end
+
+  def payment
+
+    @apr = (params["apr"].to_f)/100
+    @years = params["years"].to_i
+    @pv = params["pv"].to_i
+    @payment = ((@apr/100/12*@pv)/(1-(1+@apr/100/12)**(-(@years*12)))).round(2)
+
+  render("payment.html.erb")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:number", { :controller => "calculations", :action => "square" })
+  get("/square_root/:number2", { :controller => "calculations", :action => "square_root" })
+  get("/random/:min/:max", { :controller => "calculations", :action => "min_max" })
+  get("/payment/:apr/:years/:pv", { :controller => "calculations", :action => "payment" })
+
 end
